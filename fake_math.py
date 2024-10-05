8782a51db07cacc9c220732f24a273e9e0ebaa6b
Alena <89519703610alena@gmail.com>
2024-10-05 20:32:46 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,17 @@
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+
+    else:
+        first / second
+        result = first / second
+        return result
+
+
+
+
+
+
+
+
+
